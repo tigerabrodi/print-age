cda6719b2a85e8d2d5c4451776c89ff5772160e7
tigerabrodi <tigerabrodi@gmail.com>
2022-01-03 07:53:53 +0100

    test: add test for the parse function

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,15 @@ use std::num::ParseIntError;
 pub fn parse_age(age: &mut String) -> Result<u32, ParseIntError> {
     age.trim().parse()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_parse_age() {
+        let mut age = String::from("2");
+
+        assert_eq!(Ok(2), parse_age(&mut age))
+    }
+}
