3b645df558278b9dea92b2f9f761b3d0da2e0fd2
tigerabrodi <tigerabrodi@gmail.com>
2022-01-03 07:34:19 +0100

    feat: add initial happy path of print age

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{io, num::ParseIntError};
+
 fn main() {
-    println!("Please enter your age");
+    loop {
+     println!("Please enter your age");
+
+     
+         let mut age = String::new();
+     
+         io::stdin().read_line(&mut age).expect("Failed to read line!");
+
+         let age: Result<u32, ParseIntError> = age.trim().parse();
+     
+         match age {
+             Ok(age) => {
+                 println!("Your age is {}", age);
+                 break;
+             },
+             Err(error) => {
+                 println!("Something went wrong, please make sure have typed a number: {}", error);
+                 continue;
+             }
+         }
+    }
 }
